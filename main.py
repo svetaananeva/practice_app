47d5244d216b2f6e2e385ca462935e0d5960db3b
Sveta <svetaananeva14@gmail.com>
2025-07-15 14:25:32 +0300

    Валидация времени

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
 from dialog import Dialog
+import datetime
 
 d = Dialog()
 d.set_background_title("Бронирование компьютера")
@@ -20,25 +21,48 @@ def save_bookings(bookings):
 def book_slot():
     bookings = load_bookings()
 
-    code, time_input = d.inputbox("Введите время бронирования (например, 15:00-16:00):")
+    code, time_input = d.inputbox("Введите время бронирования (например, 10-12):")
     if code != d.OK:
         d.msgbox("Бронирование отменено.", width=40)
         return
 
     time_input = time_input.strip()
+
     if not time_input:
         d.msgbox("Пустой ввод недопустим.", width=40)
         return
 
+    parts = time_input.split("-")
+    if len(parts) != 2 or not all(p.strip().isnumeric() for p in parts):
+        d.msgbox("Неверный формат. Введите часы через тире, например: 10-12", width=50)
+        return
+
+    start, end = map(int, parts)
+    if not (0 <= start < 24 and 0 < end <= 24 and start < end):
+        d.msgbox("Введите корректный диапазон часов.", width=50)
+        return
+
     if time_input in bookings:
-        d.msgbox(f"Время '{time_input}' занято. Выберите другое.", width=50)
+        d.msgbox(f"Время '{time_input}' уже занято. Выберите другое.", width=50)
         return
 
     bookings.append(time_input)
     save_bookings(bookings)
     d.msgbox(f"Время '{time_input}' забронировано.", width=50)
 
+def validate_booking():
+    bookings = load_bookings()
+    valid_bookings = []
+    now_hour = datetime.datetime.now().hour
+    for i in bookings:
+        parts = i.strip().split("-")
+        start, end = map(int, parts)
+        if 0 <= start < 24 and 0 < end <= 24 and start < end and end > now_hour:
+            valid_bookings.append(f"{start}-{end}")
+
+
 def main():
+    validate_booking()
     while True:
         code, tag = d.menu("Меню:", choices=[
             ("1", "Забронировать слот"),
